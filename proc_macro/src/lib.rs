83a69f7ba4acab9405bf935f7dfc70f2f5279c40
Vladas <146100@gmail.com>
2023-10-24 19:06:32 +0300

    added support for promise

--- proc_macro/src/lib.rs
+++ proc_macro/src/lib.rs
@@ -2,8 +2,7 @@ extern crate proc_macro;
 
 use proc_macro::TokenStream;
 use quote::{quote, ToTokens};
-use syn::{parse_macro_input, FnArg, ItemTrait, ReturnType, TraitItem, TraitItemFn};
-use syn::{parse_str, Ident};
+use syn::{parse_macro_input, parse_str, FnArg, Ident, ItemTrait, ReturnType, TraitItem, TraitItemFn};
 
 /// Create interface trait suitable for usage in integration tests
 #[proc_macro_attribute]
@@ -12,10 +11,7 @@ pub fn make_integration_version(_args: TokenStream, stream: TokenStream) -> Toke
 
     let trait_name = &input.ident;
 
-    let async_trait_name = Ident::new(
-        &format!("{}Integration", trait_name.to_string()),
-        trait_name.span(),
-    );
+    let async_trait_name = Ident::new(&format!("{trait_name}Integration"), trait_name.span());
 
     let async_methods = input.items.iter().filter_map(|item| {
         if let TraitItem::Fn(method) = item {
@@ -55,6 +51,13 @@ fn asyncify_method(mut method: TraitItemFn) -> proc_macro2::TokenStream {
         let self_arg: FnArg = parse_str("&self").unwrap();
         method.sig.inputs.insert(0, self_arg);
         ret = "()".to_string();
+    }
+
+    if ret.starts_with("PromiseOrValue <") {
+        let start = ret.find('<').unwrap();
+        let end = ret.find('>').unwrap();
+
+        ret = ret[start + 1..end].to_string();
     }
 
     let ret: Result<ReturnType, _> = parse_str(&format!("-> anyhow::Result<{ret}>"));
--- tests/src/lib.rs
+++ tests/src/lib.rs
@@ -1,5 +1,6 @@
 use async_trait::async_trait;
 use integration_trait::make_integration_version;
+use near_sdk::{json_types::U128, PromiseOrValue};
 
 #[make_integration_version]
 pub trait ContractNameInterface {
@@ -8,22 +9,24 @@ pub trait ContractNameInterface {
 
     fn receive_name(&self) -> String;
     fn set_name(&mut self, name: String);
+
+    fn burn(&mut self) -> PromiseOrValue<U128>;
 }
 
 impl ContractNameInterface for () {
-    fn init() -> Self {
-        ()
-    }
+    fn init() -> Self {}
 
-    fn initialize_with_name(_name: String) -> Self {
-        ()
-    }
+    fn initialize_with_name(_name: String) -> Self {}
 
     fn receive_name(&self) -> String {
         Default::default()
     }
 
     fn set_name(&mut self, _name: String) {}
+
+    fn burn(&mut self) -> PromiseOrValue<U128> {
+        todo!()
+    }
 }
 
 #[async_trait]
@@ -42,5 +45,9 @@ impl ContractNameInterfaceIntegration for () {
 
     async fn set_name(&mut self, _name: String) -> anyhow::Result<()> {
         Ok(())
+    }
+
+    async fn burn(&mut self) -> anyhow::Result<U128> {
+        todo!()
     }
 }
